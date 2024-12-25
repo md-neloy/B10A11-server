d8f23002ec1f23f9ee695c57e0a33556922c03c9
mhneloy <mhneloy708@gmail.com>
2024-12-26 01:39:48 +0600

    add verfiyToken in the deatils api

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/details/:id", async (req, res) => {
+    app.get("/details/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await marathonCollection.findOne(query);
